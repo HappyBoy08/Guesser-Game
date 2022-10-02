d7bad1b65608844f19e7bbe9901e343b00b415e6
Sanjay <114865567+Sanju8888@users.noreply.github.com>
2022-10-03 00:44:50 +0530

    Update and rename README.md to Game.java
    This is a Fun Game and which is explained with 5 objects with their respective properties and behaviours and even you can modify the changes of objects as per your choice with the above example of sample Guesser code.

---
+++ Game.java
@@ -0,0 +1,88 @@
+# Guesser-Game
+//This is a Fun Guesser Game App
+
+import java.util.Scanner;
+class Guesser
+{
+	int gnum();
+	
+	
+	int guessingNum()
+	{
+		System.out.println("Guesser,Kindly guess a number..!");
+		Scanner sc=new Scanner(System.in);
+        gnum=scan.nextInt();
+        return gnum;
+	}
+	
+}
+	
+class Player
+	{
+		int pnum();
+		
+		int PredictingNum()
+		{
+			System.out.println("Player,Kindly Predict a number...!");
+			Scanner sc=new Scanner(System.in);
+			pnum=scan.nextInt();
+			return pnum;
+		}
+		
+    }
+    
+class Umpire
+  {
+    	int numFromGuesser();
+    	int numFromPlayer1();
+    	int numFromPlayer2();
+    	int numFromPlayer3();
+    	
+    	void CollectNumFromGuesser()
+    	{
+    		Guesser g=new Guesser();
+    		numFromGuesser=g.Guessernum();
+    	}
+    	void CollectNumFromPlayers()
+    	{
+    		Player P1=new Player();
+    		Player P2=new Player();
+    		Player P3=new Player();
+    		numFromPlayer1=P1.PredictingNum();
+    		numFromPlayer2=P2.PredictingNum();
+    		numFromPlayer3=P3.PredictingNum();
+    	}
+    	void compare()
+    	{
+    		if(numFromPlayer 1 ==numFromGuesser)
+    		{
+    			System.out.println("Player 1 has won the game...!");
+    		}
+    		
+    		else if(numFromPlayer 2 ==numFromGuesser)
+    		{
+    			System.out.println("Player 2 has won the game...!");
+    		}
+    		
+    		else if(numFromPlayer 3 ==numFromGuesser)
+    		{
+    			System.out.println("Player 3 has won the game...!");
+    		}
+    		
+    		
+    		else
+    		{
+    			System.out.println("Guesser lost. Try Again...!");
+    		}
+    	 }
+  }  
+    class GuesserGameApp
+    {
+    	public static void main(String[] args)
+    	{
+    		Umpire u= new Umpire();
+    		u.CollectNumFromGuesser();
+    		u.CollectNumFromPlayers();
+    		u.Compare();
+    	}
+    }
