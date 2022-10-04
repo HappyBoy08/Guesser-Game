cb656308b620eadb9a3b00e57e1b8fa9b6cc3dfc
Sanjay <114865567+Sanjay008-art@users.noreply.github.com>
2022-10-04 23:53:14 +0530

    Update Game.java

--- Game.java
+++ Game.java
@@ -2,90 +2,168 @@
 //This is a Fun Guesser Game App
 
 import java.util.Scanner;
+
 class Guesser
+
 {
-	int gnum();                                                                                           
-	
-	
-	int guessingNum()
-	{
-		System.out.println("Guesser,Kindly guess a number..!");
-		Scanner sc=new Scanner(System.in);
-        gnum=scan.nextInt();
-        return gnum;
-	}
-	
-}
-	
-class Player
-	{
-		int pnum();
-		
-		int PredictingNum()
-		{
-			System.out.println("Player,Kindly Predict a number...!");
-			Scanner sc=new Scanner(System.in);
-			pnum=scan.nextInt();
-			return pnum;
-		}
-		
-    }
+
+    int gnum;
+
     
-class Umpire
-  {
-    	int numFromGuesser();
-    	int numFromPlayer1();
-    	int numFromPlayer2();
-    	int numFromPlayer3();
-    	
-    	void CollectNumFromGuesser()
-    	{
-    		Guesser g=new Guesser();
-    		numFromGuesser=g.Guessernum();
-    	}
-    	void CollectNumFromPlayers()
-    	{
-    		Player P1=new Player();
-    		Player P2=new Player();
-    		Player P3=new Player();
-    		numFromPlayer1=P1.PredictingNum();
-    		numFromPlayer2=P2.PredictingNum();
-    		numFromPlayer3=P3.PredictingNum();
-    	}
-    	void compare()
-    	{
-    		if(numFromPlayer 1 ==numFromGuesser)
-    		{
-    			System.out.println("Player 1 has won the game...!");
-    		}
-    		
-    		else if(numFromPlayer 2 ==numFromGuesser)
-    		{
-    			System.out.println("Player 2 has won the game...!");
-    		}
-    		
-    		else if(numFromPlayer 3 ==numFromGuesser)
-    		{
-    			System.out.println("Player 3 has won the game...!");
-    		}
-    		
-    		
-    		else
-    		{
-    			System.out.println("Guesser lost. Try Again...!");
-    		}
-    	 }
-  }  
-    class GuesserGameApp
+
+    int guessingNum()
+
     {
-    	public static void main(String[] args)
-    	{
-    		Umpire u= new Umpire();
-    		u.CollectNumFromGuesser();
-    		u.CollectNumFromPlayers();
-    		u.Compare();
-    	}
+
+        System.out.println("Guesser, Guess a number :");
+
+        Scanner scan = new Scanner(System.in);
+
+        gnum=scan.nextInt();
+
+        return gnum;
+
     }
+
+}
+
+
+
+class Player
+
+{
+
+    int pnum;
+
+    
+
+    int predictingNum()
+
+    {
+
+        System.out.println("Player, Predict a number :");
+
+        Scanner scan = new Scanner(System.in);
+
+        pnum=scan.nextInt();
+
+        return pnum;
+
+    }
+
+}
+
+
+
+class Umpire
+
+{
+
+    int numGuesser;
+
+    int numPlayer1;
+
+    int numPlayer2;
+
+    int numPlayer3;
+
+    
+
+    void collectingNumGuesser()
+
+    {
+
+        Guesser g= new Guesser();
+
+        numGuesser=g.guessingNum();
+
+    }
+
+    
+
+    void collectingNumPlayers()
+
+    {
+
+        Player p1= new Player();
+
+        Player p2= new Player();
+
+        Player p3= new Player();
+
+        numPlayer1=p1.predictingNum();
+
+        numPlayer2=p2.predictingNum();
+
+        numPlayer3=p3.predictingNum();
+
+        
+
+    }
+
+    
+
+    void compare()
+
+    {
+
+    if(numPlayer1==numGuesser)
+
+    {
+
+    System.out.println("Player 1 Wins!");
+
+    }
+
+    else if(numPlayer2==numGuesser)
+
+    {
+
+    System.out.println("Player 2 Wins!");
+
+    }
+
+    else if(numPlayer3==numGuesser)
+
+    {
+
+    System.out.println("Player 3 Wins!");
+
+    }
+
+    else
+
+    {
+
+    System.out.println("Game Lost,Try again!");
+
+    }
+
+}
+
+}
+
+    
+
+class Main
+
+{
+
+    public static void main(String[] args)
+
+    {
+
+        Umpire u= new Umpire();
+
+        u.collectingNumGuesser();
+
+        u.collectingNumPlayers();
+
+        u.compare();
+
+    }
+
+}
 //output: 
 // Guesser,Kindly guess a number..!: 
 // 8
